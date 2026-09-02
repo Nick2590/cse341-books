128e730632e6cbaf6fd4db0e5502617a450be0e4
Nick2590 <pacegoodsell@gmail.com>
2026-09-01 21:06:04 -0700

    db connection setup

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { getDb } from "./src/db/connect.js";
 
 const app = express();
 
@@ -6,6 +7,12 @@ app.use(express.json());
 
 app.get("/", (request, response) => {
   response.status(200).json({ message: "Books API is running" });
+});
+
+app.get("/trails", async (request, response) => {
+  const database = getDb();
+  const trails = await database.collection("trails").find().toArray();
+  return response.status(200).json(trails);
 });
 
 export default app;
