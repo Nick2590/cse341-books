02ee47098cdc766e42f19788d30b2ab0bc0a3a83
Nick2590 <pacegoodsell@gmail.com>
2026-09-02 19:30:15 -0700

    Added GET books endpoint

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 import express from "express";
-import { getDb } from "./src/db/connect.js";
+import router from "./src/router.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(router);
 
 app.get("/", (request, response) => {
-  response.status(200).json({ message: "Books API is running" });
-});
-
-app.get("/trails", async (request, response) => {
-  const database = getDb();
-  const trails = await database.collection("trails").find().toArray();
-  return response.status(200).json(trails);
+  return response.status(200).json({ message: "Books API is running" });
 });
 
 export default app;
